0d71ed874cb2d0a9322a0e46800dc7be89c19768
Thanchanok <aimmythanchanok37530@gmail.com>
2025-09-30 15:57:17 +0700

    unit test

---
+++ dist/Utils.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function Hello() {
+    console.log("Hello, world!");
+}
+function Add(a, b) {
+    return a + b;
+}
+const Utils = {
+    Hello,
+    Add,
+};
+module.exports = Utils;
+//# sourceMappingURL=Utils.js.map
